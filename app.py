e1249255aeed6f39cc00ecc9df92300144f5cf1b
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-15 21:01:16 +0100

    fixed a bug

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime as dt
-from options_analyzer import OptionsAnalyzer
+from option import OptionsAnalyzer
 
 
 st.set_page_config(
